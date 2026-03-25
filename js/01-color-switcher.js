d320ee4306b7493fcd9015dffa65d737d767f357
sheremet <danasevcenko695@gmail.com>
2026-03-26 00:15:10 +0200

    Update 01-color-switcher.js

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,6 +1,6 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
-const body = document.body;
+const bodyEl = document.body;
 
 let timerId = null;
 
@@ -9,20 +9,26 @@ stopBtn.disabled = true;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
 
 startBtn.addEventListener('click', () => {
+  if (timerId) {
+    return;
+  }
+
   startBtn.disabled = true;
   stopBtn.disabled = false;
 
   timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
+  timerId = null;
+
   startBtn.disabled = false;
   stopBtn.disabled = true;
 });
